fcde17d5abbab518b78ba3132fe0ccc105468eb0
maesha <maesha@gmail.com>
2025-09-16 11:39:42 +0530

    sub code written

--- solve.c
+++ solve.c
@@ -3,14 +3,20 @@
 int add(int a, int b);
 
 //add the substraction code --maesha
+int sub(int a,int b);
 // add the multiplaction code --cayden
 
 int main(){
     int value=add(5,5);
     printf("%d",value);
+     int subvalue=sub(10,5);
+      printf("\n %d",subvalue);
 
 
 }
 int add(int a ,int b){
     return a+b;
+}
+int sub(int a , int b){
+return a-b;
 }
