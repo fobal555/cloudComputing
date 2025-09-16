6691f5638600b808c6b4301b5fd82f2e5121e448
Jeshinder <2305114.rcca@gmail.com>
2025-09-16 12:25:30 +0530

    mul added

--- solve.c
+++ solve.c
@@ -5,12 +5,14 @@ int add(int a, int b);
 //add the substraction code --maesha
 int sub(int a,int b);
 // add the multiplaction code --cayden
-
+int mul(int a, int b);
 int main(){
     int value=add(5,5);
     printf("%d",value);
      int subvalue=sub(10,5);
       printf("\n %d",subvalue);
+int val=mul(45,890);
+printf("mul = %d",val);
 
 
 }
@@ -19,4 +21,7 @@ int add(int a ,int b){
 }
 int sub(int a , int b){
 return a-b;
+}
+int mul(int a, int b){
+return a*b;
 }
